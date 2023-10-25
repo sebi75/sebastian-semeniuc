e83a38877efb2eccb7b1f8c562d55886f3ed3695
Sebastian Semeniuc <xsebyyy@gmail.com>
2023-10-25 09:18:20 +0300

    add social icons tooltip component and fix some bugs

--- src/app/components/Socials.tsx
+++ src/app/components/Socials.tsx
@@ -1,52 +1,76 @@
+"use client";
 import { FunctionComponent } from "react";
-import { Github, Linkedin, Twitter } from "lucide-react";
+import { Github, Linkedin, LucideIcon, Twitter } from "lucide-react";
 import { cn } from "@/lib/utils";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/app/components/ui/tooltip";
 
-type Props = {
-  className?: string;
+type SocialLink = {
+  name: string;
+  url: string;
+  Icon: LucideIcon;
+};
+
+const links: SocialLink[] = [
+  {
+    name: "Github",
+    url: "https://github.com/sebi75",
+    Icon: Github,
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/sebastian-semeniuc-17136321a/",
+    Icon: Linkedin,
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com/sebyss7",
+    Icon: Twitter,
+  },
+];
+
+type Config = {
   github?: boolean;
   linkedin?: boolean;
   twitter?: boolean;
 };
 
+type Props = {
+  className?: string;
+  config: Config;
+};
+
 export const Socials: FunctionComponent<Props> = ({
   className,
-  github = true,
-  linkedin = true,
-  twitter = true,
+  config = {
+    github: true,
+    linkedin: true,
+    twitter: true,
+  },
 }) => {
   return (
     <div className={cn("flex items-center", className)}>
-      {github && (
-        <a
-          href="https://github.com/sebi75"
-          className="mx-2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Github className="h-6 w-6 " />
-        </a>
-      )}
-      {linkedin && (
-        <a
-          href="https://www.linkedin.com/in/sebastian-semeniuc-17136321a/"
-          className="mx-2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Linkedin className="h-6 w-6 " />
-        </a>
-      )}
-      {twitter && (
-        <a
-          href="https://twitter.com/sebyss7"
-          className="mx-2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Twitter className="h-6 w-6 " />
-        </a>
-      )}
+      {links.map((link) => {
+        const { Icon, name, url } = link;
+        const key = link.name.toLowerCase() as keyof Config;
+        if (!config[key]) return null;
+        return (
+          <TooltipProvider key={`social-icon-${name}-section`}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <a href={url} className="mx-2" target="_blank" rel="noreferrer">
+                  <Icon className="h-6 w-6 " />
+                </a>
+              </TooltipTrigger>
+              <TooltipContent>{`Visit my ${name} profile`}</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        );
+      })}
     </div>
   );
 };
